e797ba6d1a70a3713e42985be77bc30399742b6a
jiakang.cjk <jiakang.cjk@aliyun.com>
2023-03-21 19:06:03 +0800

    update example

--- example/atexit.c
+++ example/atexit.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef __cplusplus              /* the __cplusplus macro is     */
-extern "C" void goodbye(void);  /* automatically defined by the  */
-#else /* C++/MVS compiler              */
-void goodbye(void);
+#ifdef __cplusplus 
+extern "C" void goodbye(void);
 #endif
+
+void goodbye(void)
+{
+    printf("The function goodbye was called at program termination\n");
+}
 
 int main(void) {
     int rc;
@@ -14,10 +17,4 @@ int main(void) {
     if (rc != 0)
         printf("Error in atexit");
     exit(0);
-}
-
-void goodbye(void)
-   /* This function is called at normal program termination */
-{
-    printf("The function goodbye was called at program termination\n");
 }
